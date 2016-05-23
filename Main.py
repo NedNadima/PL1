a8007e2b7c76e727a511618d73917e39d4f2db6c
Ned Nadima <nnadima1@gmail.com>
2016-05-22 20:03:17 -0400

    Git ignore and for loop

--- Main.py
+++ Main.py
@@ -2,9 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def test():
+    numberlist = []
+    for count in range (1,101):
+        numberlist.append(count)
+    return str(numberlist)
+        
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
-    
