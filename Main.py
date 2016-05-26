3de16a66dc3f1efa7c22bd0bb3a7799ee7960f2e
Ned Nadima <nnadima1@gmail.com>
2016-05-25 22:03:28 -0400

    Routing

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def test():
+
+@app.route("/test/<int:x>")
+def test(x):
     numberlist = []
-    for count in range (1,101):
+    for count in range (1,x):
         numberlist.append(count)
     return  ','.join(str (n) for n in numberlist)
         
@@ -12,3 +13,4 @@ def test():
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
